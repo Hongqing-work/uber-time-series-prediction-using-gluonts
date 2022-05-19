c1ee776bcbdf94882147ce4bfc5b186a23e52bf9
Hongqing-work <Shine_CHQ@outlook.com>
2022-05-19 14:29:26 +0800

    change the logic of data processing

--- src/utils/data_processor.py
+++ src/utils/data_processor.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import json
 
+# TODO(Hongqing): Add it to a configuration file and load later.
+prediction_length = 10
+
 uber_data = "./data/uber-raw-data-janjune-15.csv"
-df = pd.read_csv(uber_data,
-                 header=0,
-                 usecols=["Pickup_date", "locationID"],
-                 index_col=0)
-df.sort_index()
-df.index = pd.to_datetime(df.index)
-df = df.resample(rule='1D').mean()
-df.locationID = df.locationID.astype('int')
-start_time = pd.Timestamp(df.index[0]).strftime('%Y-%m-%d %X')
-format_dict = {"start": start_time, "target": df.locationID.values.tolist()}
-with open('./data/uber-data.json', 'w') as output_file:
-    json.dump(format_dict, output_file)
+time_series_of_locations = pd.read_csv(uber_data,
+                                       header=0,
+                                       usecols=["Pickup_date", "locationID"],
+                                       index_col=0)
+# We divide the raw data according to locationID. Each json line represents a
+# time series of a loacationID. The targets are numbers of pickup-events during
+# a day.
+time_series_of_locations = time_series_of_locations.groupby(by="locationID")
+train_path = './data/train-dataset/uber-data.json'
+test_path = './data/test-dataset/uber-data.json'
+with open(train_path, 'w') as o_train, open(test_path, 'w') as o_test:
+    for locationID, df in time_series_of_locations:
+        df.sort_index()
+        df.index = pd.to_datetime(df.index)
+        count_series = df.resample(rule='1D').size()
+        start_time = pd.Timestamp(df.index[0]).strftime('%Y-%m-%d %X')
+        format_dict = {
+            "start": start_time,
+            "target": count_series.values.tolist()
+        }
+        test_json_line = json.dumps(format_dict)
+        o_test.write(test_json_line)
+        o_test.write('\n')
+        format_dict['target'] = format_dict['target'][:-prediction_length]
+        train_json_line = json.dumps(format_dict)
+        o_train.write(test_json_line)
+        o_train.write('\n')
